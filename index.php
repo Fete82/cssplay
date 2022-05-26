0a38c3c3870df2d7149635033cd74709fd58f141
Edvin Lindahl <fete.edvin@gmail.com>
2022-05-26 17:58:54 +0200

    flag init

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSS PLAY</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="flag-container">
+            <div class="horizontal-line"></div>
+            <div class="vertical-line"></div>
+        </div>
+    </div>
+</body>
+</html>

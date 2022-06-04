033f153f0274ad8be124078a52ddabf507f65f1e
Edvin Lindahl <fete.edvin@gmail.com>
2022-06-05 00:38:56 +0200

    append this bitch

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,7 @@ btnResetText.addEventListener('click', () => {
     console.log('There and Back Again');
     paragraph.innerText = 'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.[1] CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.';
 });
+
+
+const body = document.body;
+body.append("This string was appended with JavaScript");
